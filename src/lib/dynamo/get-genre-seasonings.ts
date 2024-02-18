267c58c198344dd02b9be2348e869b956ac6e9b3
tamashiro-syuta <syuta5825@gmail.com>
2024-02-18 12:20:11 +0900

    🚧 dynamoからデータを取得する際に、定義したclassへキャストした値を返すようにした

--- src/lib/dynamo/get-genre-seasonings.ts
+++ src/lib/dynamo/get-genre-seasonings.ts
@@ -1,31 +1,71 @@
+import { Ingredient, ingredientNames } from "@/domain/ingredient";
+import { Seasoning } from "@/domain/seasoning";
 import {
+  AttributeValue,
   DynamoDBClient,
   QueryCommand,
   QueryCommandInput,
-} from '@aws-sdk/client-dynamodb'
+} from "@aws-sdk/client-dynamodb";
 
 interface Props {
   genre: string;
 }
 
-export const getGenreSeasonings = async ({ genre }: Props): Promise<any> => {
+export const getGenreSeasonings = async ({
+  genre,
+}: Props): Promise<Seasoning[]> => {
   const params: QueryCommandInput = {
-      TableName: 'seasoning-recipes',
-      IndexName: 'genre-index',
-      KeyConditionExpression: 'genre = :genre',
-      ExpressionAttributeValues: {
-        ':genre': { S: genre },
-      },
-    }
+    TableName: "seasoning-recipes",
+    IndexName: "genre-index",
+    KeyConditionExpression: "genre = :genre",
+    ExpressionAttributeValues: {
+      ":genre": { S: genre },
+    },
+  };
 
   try {
-    const dynamoDBClient = new DynamoDBClient({})
-    const result = await dynamoDBClient.send(new QueryCommand(params))
+    const dynamoDBClient = new DynamoDBClient({});
+    const result = await dynamoDBClient.send(new QueryCommand(params));
+    if (!result.Items) throw new Error("No items");
 
-    console.log(result)
-
-    return result
-    } catch (dbError) {
-      return { statusCode: 500, body: JSON.stringify(dbError) };
+    const seasonings = result.Items?.map((item) => convertToSeasoning(item));
+    return seasonings;
+  } catch (dbError) {
+    console.error("Error", dbError);
+    throw new Error();
   }
+};
+
+const convertToIngredient = (
+  item: Record<string, AttributeValue>
+): Ingredient[] => {
+  const ingredients = ingredientNames.map((ingredientName) => {
+    if (item[ingredientName.en]) {
+      return new Ingredient(
+        ingredientName.en,
+        // NOTE: 値は必ず存在するものとする(個人で使うサービスのため許容)
+        Ingredient.convertToSize(item[ingredientName.en]?.M?.size.S!)!.en,
+        Number(item[ingredientName.en]?.M?.quantity.N)
+      );
+    }
+  });
+
+  return ingredients.filter(
+    (ingredient) => ingredient !== undefined
+  ) as Ingredient[];
+};
+
+const convertToSeasoning = (
+  item: Record<string, AttributeValue>
+): Seasoning => {
+  const name = item.name.S;
+  const genre = item.genre.S;
+  const ingredients = convertToIngredient(item);
+
+  const convertedGenre = Seasoning.convertToGenre(genre || "");
+  if (!name) throw new Error("Invalid name");
+  if (!convertedGenre) throw new Error("Invalid genre");
+  if (!ingredients) throw new Error("Invalid ingredients");
+
+  return new Seasoning(name, convertedGenre, ingredients);
 };
