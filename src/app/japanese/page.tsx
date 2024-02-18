797e3c83d9ff7569ac6eed71985c8c98ae348762
tamashiro-syuta <syuta5825@gmail.com>
2024-02-18 15:55:18 +0900

    ✨ ジャンルごとのページを作成

--- src/app/japanese/page.tsx
+++ src/app/japanese/page.tsx
@@ -1,9 +1,37 @@
-const Page = () => {
+import SeasoningCard from "@/components/custom/seasoning-card";
+import SeasoningSelect from "@/components/custom/seasoning-select";
+import SeasoningSelectGroup from "@/components/custom/seasoning-select-group";
+import { Skeleton } from "@/components/ui/skeleton";
+import { GENRES } from "@/lib/constants";
+import { getGenreSeasonings } from "@/lib/dynamo/get-genre-seasonings";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const genre = GENRES.JAPANESE;
+  const seasonings = await getGenreSeasonings({ genre: genre });
+
   return (
-    <div>
-      <h1>ここに和風の一覧</h1>
-    </div>
+    <main className="flex flex-col items-center justify-between">
+      <div className="sticky top-10 p-2 z-50 w-full bg-white">
+        <SeasoningSelect
+          inSelectContent={
+            <Suspense
+              fallback={
+                <Skeleton className="w-full h-[20px] px-2 my-2 bg-gray-200 rounded-full" />
+              }
+            >
+              <SeasoningSelectGroup genre={genre} />
+            </Suspense>
+          }
+        />
+      </div>
+      {seasonings.map((seasoning, index) => (
+        <div key={index} className="mt-2">
+          <SeasoningCard seasoning={seasoning} />
+        </div>
+      ))}
+    </main>
   );
-}
+};
 
 export default Page;
---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const GENRES = {
+  JAPANESE: "和風",
+  WESTERN: "洋風",
+  CHINESE: "中華",
+  SIDE_DISH: "副菜",
+  DRESSING: "ドレッシング",
+  OTHER: "その他",
+} as const;
