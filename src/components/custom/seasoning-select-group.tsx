2ae241363ba9e43f4c118cbbc0ce0efafa124e77
tamashiro-syuta <syuta5825@gmail.com>
2024-02-18 16:53:47 +0900

    🔥 開発ように残していた不要なファイルやコードを削除

--- src/components/custom/seasoning-select-group.tsx
+++ src/components/custom/seasoning-select-group.tsx
@@ -1,17 +1,5 @@
 import { SelectGroup, SelectItem, SelectLabel } from "@/components/ui/select";
-import { Seasoning } from "@/domain/seasoning";
 import { getGenreSeasonings } from "@/lib/dynamo/get-genre-seasonings";
-
-const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-async function fetchData1(genre: string): Promise<Seasoning[]> {
-  // ランダム秒数待機
-  await sleep(Math.floor(Math.random() * 3) * 1000);
-  const seasonings = await getGenreSeasonings({ genre: genre });
-  return seasonings;
-}
 
 interface Props {
   genre: string;
@@ -19,7 +7,7 @@ interface Props {
 }
 
 const SeasoningSelectGroup = async ({ genre, withLabel = false }: Props) => {
-  const seasonings = await fetchData1(genre);
+  const seasonings = await getGenreSeasonings({ genre: genre });
   if (seasonings.length === 0) return;
 
   return (
